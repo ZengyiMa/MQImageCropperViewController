affb16478c72cc38daafeb04e15792f66ba17d58
mazengyi <zyma@myfansz.com>
2016-02-16 09:17:17 +0800

    Initial Commit

---
+++ MQImageCropperViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MQImageCropperViewController
+//
+//  Created by mazengyi on 16/2/16.
+//  Copyright © 2016年 fansz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
